343c475dea1d877df45b250e5cd4d2d110793efe
Karen Ngo <uminhngo@gmail.com>
2023-06-21 08:37:11 -0400

    Initial commit

---
+++ src/main/java/DatabaseConnector.java
@@ -0,0 +1,52 @@
+import java.sql.*;
+
+public class DatabaseConnector {
+    public Connection connect() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/Bookstore";
+            String username = "root";
+            String password = "password";
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Successfully connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void selectAllBooks() {
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            connection = this.connect();
+            String sql = "SELECT * FROM books";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("title") + "\t" +
+                        rs.getString("author") + "\t" +
+                        rs.getDouble("price"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error executing SELECT statement");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DatabaseConnector connector = new DatabaseConnector();
+        connector.selectAllBooks();
+    }
+}
